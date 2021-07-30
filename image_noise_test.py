8af81fccd6fa03497d678be2295c841dece01320
ddl06024 <ddl06024@naver.com>
2021-07-30 15:46:45 +0900

    finish image noise

--- image_noise_test.py
+++ image_noise_test.py
@@ -6,10 +6,13 @@ import numpy as np
 # transform된 이미지 저장
 def image_save(path, filename, img_list):
 
+    print("image_save " + filename)
+
     for i in range(1, len(img_list) + 1):
         img = cv2.imread(path + '/' + filename + str(i) + '.jpg')
+        print(type(img))
 
-        if img.all() == None:
+        if str(type(img)) == "<class 'NoneType'>":
             cv2.imwrite(path + '/' + filename + str(i) + '.jpg', img_list[i - 1])
         else: 
             print("already exist")
@@ -46,20 +49,31 @@ def rotate_img(img_list):
 
 # image mirror
 def mirror_img(img_list):
-    
     img_mirror = [cv2.flip(img, 1) for img in img_list]
     return img_mirror
 
+# image bright
+def dark_img(img_list):
+    # 50정도 어둡게
+    dark = [np.ones(img.shape,dtype="uint8") * 50 for img in img_list]
+    
+    img_dark = []
+
+    for i in range(0, len(img_list)):
+        img_dark.append(cv2.subtract(img_list[i], dark[i]))
+
+    return img_dark    
+
+# image dark
 def bright_img(img_list):
 
-    bright = [ np.ones(img.shape,dtype="uint8") for img in img_list]
-    print(bright)
-
+    bright = [ np.ones(img.shape,dtype="uint8") * 100 for img in img_list]
     img_bright = []
 
-    for idx,img in enumerate(img_list):
-        print(idx)
-        print(img)
+    # 100 밝기 높이기
+    for i in range(0, len(img_list)):
+        img_bright.append(cv2.add(img_list[i], bright[i]))
+
     return img_bright
 
 if __name__ == '__main__':
@@ -69,6 +83,7 @@ if __name__ == '__main__':
     path_rotate = './data/rotate'
     path_mirror = './data/mirror'
     path_bright = './data/bright'
+    path_dark = './data/dark'
 
     img_train = image_file(path_train, 100)
     print(len(img_train))
@@ -99,15 +114,35 @@ if __name__ == '__main__':
     img_bright = bright_img(img_train)
     print(len(img_bright))
 
-    # debug
+    '''debug
     plt.imshow(img_bright[0])
     plt.show()
-    
+    '''
 
-    ''' image save
+    # image dark
+    img_dark = dark_img(img_train)
+    print(len(img_dark))
+
+    # debug
+    plt.imshow(img_dark[0])
+    plt.show()
+    
+    print("image save start")
+    #image save
     name_rot = "rotate"
     image_save(path_rotate, name_rot , img_rot)
-    '''
+
+    name_mirror = "mirror"
+    image_save(path_mirror, name_mirror, img_mirror)
+
+    name_bright = "bright"
+    image_save(path_bright, name_bright, img_bright)
+
+    name_dark = "dark"
+    image_save(path_dark, name_dark, img_dark)
+
+    print("image save finish")
+    
     
 
 
